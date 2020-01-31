da9abb7844a62d034fd69fb4f30e59790bd59f23
rafaelanicole <38031578+rafaelanicole@users.noreply.github.com>
2020-01-31 16:18:42 +0800

    added process control dashboard

---
+++ src/app/processcontrol-module/processcontrol-module.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-processcontrol-module',
+  templateUrl: './processcontrol-module.component.html',
+  styleUrls: ['./processcontrol-module.component.css']
+})
+export class ProcesscontrolModuleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
