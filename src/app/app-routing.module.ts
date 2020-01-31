60c75d37326a5540e3b1912a84f1353183023d57
Benedict <33305068+Juicy4pple@users.noreply.github.com>
2020-01-31 17:24:37 +0800

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from "./login/login.component";
 import { SalesModuleComponent } from "./sales-module/sales-module.component";
 import { ProcurementModuleComponent } from "./procurement-module/procurement-module.component";
 import { QuotationViewComponent } from './quotation-view/quotation-view.component';
@@ -13,6 +14,7 @@ import { DesignModuleComponent } from "./design-module/design-module.component";
 
 const AppRoutes: Routes = [
   { path: '', redirectTo: '/sales-module', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
   { path: 'sales-module', component: SalesModuleComponent },
   { path: 'procurement-module', component: ProcurementModuleComponent },
   { path: 'customerCreate', component: CustomerCreateComponent },
