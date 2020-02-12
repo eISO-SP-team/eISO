e859121a32340cd4b8ad601c5e9a4b55141ae12a
Poon Xiang Yuan <poonxiangyuan.17@ichat.sp.edu.sg>
2020-02-12 14:34:10 +0800

    logout done

--- src/app/nav-header/nav-header.component.ts
+++ src/app/nav-header/nav-header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ToggleDisplayService } from '../shared/service/toggle-display.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-header',
@@ -11,5 +13,7 @@ export class NavHeaderComponent implements OnInit {
 
   ngOnInit() {
   }
+
+
 
 }
