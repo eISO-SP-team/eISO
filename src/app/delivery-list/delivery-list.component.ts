35bfbc3be187203183729d70b1d5ab54dcbda39f
Poon Xiang Yuan <poonxiangyuan.17@ichat.sp.edu.sg>
2020-02-14 19:58:11 +0800

    xy reverse

--- src/app/delivery-list/delivery-list.component.ts
+++ src/app/delivery-list/delivery-list.component.ts
@@ -11,8 +11,9 @@ import { Router } from '@angular/router';
 export class DeliveryListComponent implements OnInit {
 
   deliveryList: any;
-  
+
   deliveryTest: any;
+  index: number;
 
   constructor(public deliveryService: DeliveryService, public router: Router, private confirmationservice: ConfirmationService) {
     this.deliveryService.getDeliveryListener()
@@ -20,13 +21,53 @@ export class DeliveryListComponent implements OnInit {
         this.deliveryList = newList;
         this.deliveryTest = this.deliveryService.loadDeliveries().subscribe(responseData => {
           this.deliveryService.deliveryList = (<any>responseData).body;
-          this.deliveryList = this.deliveryService.deliveryList;
+          for (let i = 0; i < this.deliveryService.deliveryList.length; i++)
+            if (this.deliveryService.deliveryList[i].purchase_id == this.deliveryService.selectedPR) {
+              console.log(this.deliveryService.deliveryList[i].purchase_id);
+              console.log(this.deliveryService.selectedPR);
+              this.deliveryList.push(this.deliveryService.deliveryList[i]);
+            }
         });
       });
 
   }
 
   ngOnInit() {
+  }
+
+
+  viewEnquiry(enquiry) {
+    console.log("In onViewDetail......" + JSON.stringify(enquiry));
+    this.deliveryService.selectedDeliveryService = enquiry;
+    this.router.navigate(['/doView', enquiry.id]);
+  }
+
+  deleteEnquiry(enquiry) {
+    console.log("Delete this enquiry......" + JSON.stringify(enquiry.id));
+    this.deliveryService.deleteDeliveries(enquiry.id).subscribe(() => {
+      for (let i = 0; i < this.deliveryList.length; i++) {
+        if (this.deliveryList[i].id == enquiry.id) {
+          this.index = i;
+          break;
+        }
+      }
+      this.deliveryList.splice(this.index, 1);
+      console.log('Vendor with id: ' + enquiry.id + ' has been deleted');
+    });
+
+  }
+
+  confirm(enquiry) {
+    this.confirmationservice.confirm({
+      message: 'Are you sure that you want to proceed?',
+      header: 'Confirmation',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.deleteEnquiry(enquiry);
+      },
+      reject: () => {
+      }
+    })
   }
 
 }
